9b24ed3a6f5cf4f65a544aa93549f2b99e25f814
musseGkel <flutterdev6ird@gmail.com>
2023-11-21 00:17:30 +0100

    initial commit

---
+++ lib/my_home_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Penny Path"),
+      ),
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            height: 120,
+            width: 120,
+            color: Colors.red,
+          ),
+        ],
+      ),
+    );
+  }
+}
